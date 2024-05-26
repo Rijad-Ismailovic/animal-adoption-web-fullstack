140269adcbf384163a2116c2a1e5ebc2b8d54116
Rijad-Ismailovic <rijad.isma@gmail.com>
2024-05-26 18:23:49 +0200

    Samo dodao / na kraju API_BASE_URL

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -3,7 +3,7 @@ var Constants = {
     if (location.hostname == "localhost") {
       return "http://localhost/web-project/backend/";
     } else {
-      return "https://coral-app-pxup9.ondigitalocean.app/backend";
+      return "https://coral-app-pxup9.ondigitalocean.app/backend/";
     }
   }
   //API_BASE_URL: "http://localhost/web-project/backend/",
