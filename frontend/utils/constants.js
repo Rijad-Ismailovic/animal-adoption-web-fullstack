acaf4563feefed6f97e21b0520c840a893ab2fe9
Rijad-Ismailovic <rijad.isma@gmail.com>
2024-05-29 09:34:35 +0200

    Dino fix

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -3,7 +3,7 @@ var Constants = {
     if (location.hostname == "localhost") {
       return "http://localhost/web-project/frontend/backend/";
     } else {    
-      return "https://https://stingray-app-2eyro.ondigitalocean.app/backend/";
+      return "https://stingray-app-2eyro.ondigitalocean.app/backend/";
     }
   }
   //API_BASE_URL: "http://localhost/web-project/backend/",
