b13c877028197e4161adf4878b513856b45be853
Rijad-Ismailovic <rijad.isma@gmail.com>
2024-05-29 09:16:45 +0200

    infopho

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-
-require "./vendor/autoload.php";
-require __DIR__."/rest/routes/middleware_routes.php";
-require __DIR__."/rest/routes/animal_routes.php";
-require __DIR__."/rest/routes/user_routes.php";
-require __DIR__."/rest/routes/auth_routes.php";
-
-Flight::start();
+<?
+    phpinfo();
+?>
