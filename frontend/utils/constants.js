aa926fa1c7a986f34e3f089bb39e9c11933e83b1
Rijad-Ismailovic <rijad.isma@gmail.com>
2024-05-29 09:28:53 +0200

    aaa

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -3,7 +3,7 @@ var Constants = {
     if (location.hostname == "localhost") {
       return "http://localhost/web-project/frontend/backend/";
     } else {    
-      return "https://coral-app-pxup9.ondigitalocean.app/backend/";
+      return "https://https://stingray-app-2eyro.ondigitalocean.app/backend/";
     }
   }
   //API_BASE_URL: "http://localhost/web-project/backend/",
